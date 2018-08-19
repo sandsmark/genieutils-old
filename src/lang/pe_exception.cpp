3d5aca6f5b671c8d3dff450c89825ae7be9b2ea3
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2018-08-19 16:24:54 +0200

    run clang-format on the new pe lib

--- src/lang/pe_exception.cpp
+++ src/lang/pe_exception.cpp
@@ -1,19 +1,20 @@
 #include "pe_exception.h"
 
-namespace pe_bliss
-{
+namespace pe_bliss {
 //PE exception class constructors
-pe_exception::pe_exception(const char* text, exception_id id)
-	:std::runtime_error(text), id_(id)
-{}
+pe_exception::pe_exception(const char *text, exception_id id) :
+    std::runtime_error(text), id_(id)
+{
+}
 
-pe_exception::pe_exception(const std::string& text, exception_id id)
-	:std::runtime_error(text), id_(id)
-{}
+pe_exception::pe_exception(const std::string &text, exception_id id) :
+    std::runtime_error(text), id_(id)
+{
+}
 
 //Returns exception ID
 pe_exception::exception_id pe_exception::get_id() const
 {
-	return id_;
+    return id_;
 }
 }
